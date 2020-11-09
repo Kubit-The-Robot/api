61740436d347711806a2144abd36fbec2ace9051
EnzoBtv <enzo_batrov@hotmail.com>
2020-11-08 23:45:19 -0300

    fix: remove token auth

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -6,14 +6,14 @@ const logger = require("../util/logger");
 
 module.exports = (req, res, next) => {
 	try {
-		if (!req.headers || !req.headers.token) {
-			logger.error("Authentication error, token not found in the headers");
-			res.status(UNAUTHORIZED).json({
-				error:
-					"O token utilizado está inválido, por favor, refaça seu login"
-			});
-		}
-		req.id = jwt.verify(req.headers.token, process.env.SECRET).id;
+		// if (!req.headers || !req.headers.token) {
+		// 	logger.error("Authentication error, token not found in the headers");
+		// 	res.status(UNAUTHORIZED).json({
+		// 		error:
+		// 			"O token utilizado está inválido, por favor, refaça seu login"
+		// 	});
+		// }
+		// req.id = jwt.verify(req.headers.token, process.env.SECRET).id;
 		return next();
 	} catch (ex) {
 		res.status(UNAUTHORIZED).json({
