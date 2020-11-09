caa93864d2d1436182c8483d9bde642f7690ea88
EnzoBtv <enzo_batrov@hotmail.com>
2020-11-08 23:55:08 -0300

    fix: changing token to be the user email

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -1,18 +1,36 @@
 const jwt = require("jsonwebtoken");
 
+const Client = require("../database/models/User");
+
 const { UNAUTHORIZED } = require("../constants/HttpStatus");
 
 const logger = require("../util/logger");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
 	try {
-		// if (!req.headers || !req.headers.token) {
-		// 	logger.error("Authentication error, token not found in the headers");
-		// 	res.status(UNAUTHORIZED).json({
-		// 		error:
-		// 			"O token utilizado está inválido, por favor, refaça seu login"
-		// 	});
-		// }
+		if (!req.headers || !req.headers.token) {
+			logger.error("Authentication error, token not found in the headers");
+			res.status(UNAUTHORIZED).json({
+				error:
+					"O token utilizado está inválido, por favor, refaça seu login"
+			});
+		}
+
+		const client = await Client.findOne({
+			where: {
+				email: req.headers.token
+			}
+		})
+		if (!client) {
+			logger.error("Authentication error, token not found in the headers");
+			res.status(UNAUTHORIZED).json({
+				error:
+					"O token utilizado está inválido, por favor, refaça seu login"
+			});
+		}
+
+		req.id = client.id
+
 		// req.id = jwt.verify(req.headers.token, process.env.SECRET).id;
 		return next();
 	} catch (ex) {
